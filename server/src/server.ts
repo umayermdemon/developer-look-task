370d29fe4fcbbdcc7a96f6de81e2277352150cd6
umayermdemon <mamudmdemon@gmail.com>
2025-10-01 08:13:12 +0600

    feat: mongoose install and config file setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,18 @@
+import mongoose from "mongoose";
+import { Server } from "http";
 import app from "./app";
+import config from "./app/config";
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+let server: Server;
+async function main() {
+  try {
+    await mongoose.connect(config.mongoURI as string);
+    server = app.listen(port);
+    console.log(`Server is running on http://localhost:${port}`);
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+main();
